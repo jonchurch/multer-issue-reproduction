6bbe8bef9f1b27f58b6c313e596127f84a64c871
Jon Church <me@jonchurch.com>
2020-05-21 16:27:44 -0400

    checkin example

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 const express = require('express');
 const multer = require('multer');
+const getStream = require('get-stream');
+const axios = require('axios');
+const FormData = require('form-data');
+
+const CLIENT_ID = process.env.IMGUR_ID;
+// console.log('Imgur id:', CLIENT_ID);
 
 const upload = multer();
 
@@ -7,9 +13,29 @@ const app = express();
 
 app.use(express.static('public'));
 
-app.post('/file', upload.single('file'), (req, res, next) => {
-  console.log(req.file);
-  res.send(req.file || 'no file submitted');
+app.post('/file', upload.single('file'), async (req, res, next) => {
+  try {
+    const imgurForm = new FormData();
+    imgurForm.append('image', req.file.stream);
+    const result = await axios.post(
+      'https://api.imgur.com/3/upload',
+      imgurForm,
+      {
+        headers: {
+          Authorization: `Client-ID ${CLIENT_ID}`,
+          ...imgurForm.getHeaders(),
+        },
+      }
+    );
+
+    console.log(result.data);
+    //
+
+    res.send(result.data);
+  } catch (err) {
+    console.log(err);
+    next(err);
+  }
 });
 
 const PORT = process.env.PORT || 3000;
